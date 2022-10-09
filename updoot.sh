675e113779b62d83206e1f0f911bf3644898c779
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2022-10-09 01:53:23 -0400

    Updating my update script with some echos like I had on my elementary OS machine

--- updoot.sh
+++ updoot.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+echo "Running \`sudo dnf upgrade --refresh\`:"
 sudo dnf upgrade --refresh
+
+echo "Running \`dnf autoremove\`:"
 sudo dnf autoremove
+
+echo "Runnng \`sudo flatpak update\`:"
 sudo flatpak update
+
+echo "Runnng \`sudo flatpak uninstall --unused\`:"
 sudo flatpak uninstall --unused
