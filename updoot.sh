400451ae807cdcfdfa650804a4c16af4d47a773f
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2023-01-07 00:38:49 -0500

    Changing my update script to print commands as they are executed via the bash xtrace option/flag

--- updoot.sh
+++ updoot.sh
@@ -1,13 +1,5 @@
-#!/bin/bash
-
-echo "Running \`sudo dnf upgrade --refresh\`:"
+#!/bin/bash -x
 sudo dnf upgrade --refresh
-
-echo "Running \`dnf autoremove\`:"
 sudo dnf autoremove
-
-echo "Runnng \`sudo flatpak update\`:"
 sudo flatpak update
-
-echo "Runnng \`sudo flatpak uninstall --unused\`:"
 sudo flatpak uninstall --unused
