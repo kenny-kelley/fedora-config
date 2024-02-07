4fc47f92e5e71b9eae024fb53ba0661a3540442b
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2024-02-06 23:51:41 -0500

    Removing my update scripts (since they're in my dotfiles now)

--- updoot-firmware.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -x
-fwupdmgr refresh --force
-fwupdmgr update
--- updoot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-sudo dnf upgrade --refresh
-sudo dnf autoremove
-sudo flatpak update
-sudo flatpak uninstall --unused
-flatpak update
-flatpak uninstall --unused
